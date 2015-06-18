9830f07edd37b787b28b3244cb5e094f6424586b
Ethan Teague <ethan.teague@civicactions.com>
2015-06-18 18:43:10 -0400

    Fixing logic to return all reports for drugs, as well as number reported.

--- assets/nebula.js
+++ assets/nebula.js
@@ -3,14 +3,20 @@
 	$('#submit').click(function() {
 	    $('#text').empty();
 	    var drug = $('#drug').val();
-   	    var url = 'https://api.fda.gov/drug/event.json?search=patient.drug.medicinalproduct="' + drug + '"';
-	    var reaction = [];
-	    $.get(url, function(data) {
-		var reactions = data.results[0].patient.reaction		
-		for (i = 0; i < reactions.length; i++) {
-		    $('#text').append('<div id=drug"' + i  + '">' + reactions[i].reactionmeddrapt + '</div>');		    
-		}
-	    });	    
+	    var symptom = $('#symptom').val();
+   	    var url = 'https://api.fda.gov/drug/event.json?api_key=rv4OOon6fPJOHBbFHClUOs3BRGSbAEUdg3ACp2pu&search="' + drug + '"&count=patient.reaction.reactionmeddrapt.exact';
+	    
+	    $.get(
+		url,
+		function(data) {
+		    var reactions = data.results		
+		    for (i = 0; i < reactions.length; i++) {
+			$('#text').append('<div id=drug"' + i + '">' + reactions[i]['term'] + ' |  Reports: ' + reactions[i]['count'] +'</div>');
+			
+		    }
+		    console.log(data);
+		    
+		});	    
 	})
     })
 })(jQuery);
