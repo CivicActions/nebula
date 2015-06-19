7f6f4ffe6eefd51c52211b381064062f15e3be09
Ethan Teague <ethan.teague@civicactions.com>
2015-06-18 22:12:20 -0400

    Implementing low level jQuery ajax function in-lieu of get function. This allows for me to create an error function when no results returned, and display error message to user.

--- assets/nebula.js
+++ assets/nebula.js
@@ -1,7 +1,7 @@
 (function($){
     $(document).ready(function(){
 	$('#submit').click(function() {
-	    $('#text').empty();
+	    $('#text, #error').empty();
 	    var drug = $('#drug').val();
 	    var symptom = $('#symptom').val();
 	    var url;
@@ -16,20 +16,26 @@
 	    
 	    if(drug.length && symptom.length){
 		url = 'https://api.fda.gov/drug/event.json?api_key=rv4OOon6fPJOHBbFHClUOs3BRGSbAEUdg3ACp2pu&search="' + drug + '"+AND+"' + symptom + '"&count=patient.drug.medicinalproduct.exact';
-		
 	    }	    
-	    
-	    $.get(
-		url,
-		function(data) {
+
+	    $.ajax({
+		url: url,
+		type: 'GET',
+		success: function(data) { 
 		    var reactions = data.results		
 		    for (i = 0; i < reactions.length; i++) {
 			$('#text').append('<div id=drug"' + i + '">' + reactions[i]['term'] + ' |  Reports: ' + reactions[i]['count'] +'</div>');
-			
 		    }
-		    console.log(data);
-		    
-		});	    
+		},
+		error: function(data) {
+		    $('#error').append('No results.');
+		}
+	    });
 	})
+
+	$('#add-to-list').click(function() {
+	    $('#added-meds').append('<div id=added-drug"' + i + '">' + reactions[i]['term'] + ' |  Reports: ' + reactions[i]['count'] +'</div>');
+	})
+	
     })
 })(jQuery);
