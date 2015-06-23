c05ebbf07fd31166dc6e094db0990c1613e9b619
aravenrose <raven_web@749006.no-reply.drupal.org>
2015-06-23 17:01:24 -0600

    DB connection setup, prescriptions table set up, insert from file duplicating rows

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,5 +1,10 @@
 <?php
-
+ini_set('memory_limit','500M');
+function dpr($var){
+  print '<pre>';
+  print_r($var);
+  print '</pre>';
+}
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 use GuzzleHttp\Client;
@@ -41,6 +46,62 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
         'url'   => 'mysql://nebula:nebula@mysql:3306/nebula',
     ),
 ));
-$app['db']->fetchAll('SHOW tables');
+
+
+/* Try and build our tables */
+$platform = $app['db']->getDatabasePlatform();
+
+$schema = new \Doctrine\DBAL\Schema\Schema();
+
+/* Create "Prescriptions Table" */
+try {
+  $prescribedTable = $schema->createTable("prescribed_medicines");
+  /* UNIQUE RX/PRESCRIBED MEDICINE IDENTIFIER */
+  $prescribedTable->addColumn("drugidx", "string", array("length"=> 64, ));
+  /* RX Name */
+  $prescribedTable->addColumn("rx_name", "string", array("length" => 256));
+  /* Prescription Year */
+  $prescribedTable->addColumn("drugidx_year", "integer");
+  
+  $queries = $schema->toSql($platform);
+  foreach($queries as $query){
+    $app['db']->executeQuery($query);
+  }
+}
+catch (Exception $exc) {
+}
+
+/* Populate Our prescription Table */
+
+$queryBuilder = $app['db']->createQueryBuilder();
+
+$file = '../build/h152a.dat';
+if (file_exists($file)) {
+  $data = file($file);
+  foreach($data as $row){
+    $drugidx = trim(substr($row,27,14)); // UNIQUE RX/PRESCRIBED MEDICINE IDENTIFIER
+    $rxname = trim(substr($row,65,49)); // EDICATION NAME (IMPUTED)
+    $report_year = '2012'; // Year impmorted report was for
+    // Build insert query for each row
+    /**
+     * 
+     */
+    $queryBuilder
+      ->insert('prescribed_medicines')
+      ->setValue('drugidx', "'" . $drugidx . "'")
+      ->setValue('rx_name', "'" . $rxname . "'")
+      ->setValue('drugidx_year', "'" . $report_year . "'")
+      ->execute();
+    ;
+    dpr($queryBuilder);
+    print 'end';
+    exit;
+  }
+}
+
+$schema = new \Doctrine\DBAL\Schema\Schema();
+
+print 'hello';
 
 $app->run();
+
