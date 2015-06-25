758d5247bb9f29d80b2afab1ac21cb690bcf1a5d
David Numan <david.numan@civicactions.com>
2015-06-25 12:05:15 -0400

    Adding second test: json url

--- backend/tests/APITest.php
+++ backend/tests/APITest.php
@@ -11,4 +11,11 @@ class APITest extends PHPUnit_Framework_TestCase
         $response = $request;
         $this->assertEquals($response->getStatusCode(), 200);
     }
+    public function testJSON()
+    {
+        $client = new Client();
+        $request = $client->get('http://web/rx.json');
+        $response = $request;
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
 }
