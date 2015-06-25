7abed63268fe8e8cc54cae8300d80d772804be07
aravenrose <raven_web@749006.no-reply.drupal.org>
2015-06-25 15:39:49 -0600

    Added js to trick submit on 'enter' keystroke in input field

--- frontend/web/assets/nebula.js
+++ frontend/web/assets/nebula.js
@@ -383,6 +383,12 @@
       datax.unshift(drugs);
       drawChart(sanitizedColors, datax);
     }
-    
+    $('input').keypress(function (e) {
+     var key = e.which;
+     if(key == 13)  // the enter key code
+      {
+        $('#add-to-list').click();
+      }
+    });
   })
 })(jQuery);
