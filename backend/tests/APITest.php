0a4d3d88676b3982c9166fab1ebf0fbb6b5d995d
David Numan <david.numan@civicactions.com>
2015-06-24 23:39:29 -0400

    Minor corrections

--- backend/tests/APITest.php
+++ backend/tests/APITest.php
@@ -7,7 +7,7 @@ class APITest extends PHPUnit_Framework_TestCase
     public function testIbuprofenPain()
     {
         $client = new Client();
-        $request = $client->get('http://web/drug/ibuprofen/pain');
+        $request = $client->get('http://web/rx/ibuprofen');
         $response = $request;
         $this->assertEquals($response->getStatusCode(), 200);
     }
