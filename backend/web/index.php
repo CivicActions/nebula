f370df1958bd75a57112cb2cba841d9e82dcfdd2
Owen Barton <drupal@owenbarton.com>
2015-06-29 23:25:32 -0700

    Refactoring AHRQ endpoint to use query builder, and code formatting cleanup.

--- backend/web/index.php
+++ backend/web/index.php
@@ -24,10 +24,6 @@ $app->after(function (Request $request, Response $response) {
   $response->headers->set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
 });
 
-class ahrq {
-  public $ahrq_sample = '';
-}
-
 /**
  * Make an FDA lookup for search terms, returning symptoms by incidence. 
  *
@@ -44,7 +40,7 @@ function queryFDA($search) {
   ]);
 
   $response = $client->get('https://api.fda.gov/drug/event.json', [
-    'http_errors' => false,
+    'http_errors' => FALSE,
     'query' => [
       'api_key' => 'rv4OOon6fPJOHBbFHClUOs3BRGSbAEUdg3ACp2pu',
       'search' => $search,
@@ -53,16 +49,24 @@ function queryFDA($search) {
   return $response;
 }
 
-function ahrq($term){
-  $app = new Silex\Application();
-  $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-      'db.options' => array(
-          'url'   => 'mysql://nebula:nebula@mysql:3306/nebula',
-      ),
-  ));
-  $countQuery = "SELECT COUNT(*) FROM prescribed_medicines WHERE rx_name = '$term'";
-  $count = $app['db']->fetchAll($countQuery)[0]['COUNT(*)'];
-  return $count;
+/**
+ * Lookup the stored AHRA data for a drug, returning the count.
+ *
+ * @param string $drug
+ *   A standard prescription drug name.
+ * 
+ * @return int
+ *   Count of total prescriptions in data set.
+ */
+function ahrqCount($app, $drug) {
+  $queryBuilder = $app['db']->createQueryBuilder();
+  $result = $queryBuilder
+    ->select('COUNT(*) AS total')
+    ->from('prescribed_medicines')
+    ->where('rx_name = ?')
+    ->setParameter(0, $drug)
+    ->execute();
+  return $result->fetchAll()[0]['total'];
 }
 
 // API endpoint that provides a simplified interface to the FCC API,
@@ -74,57 +78,20 @@ $app->get('/rx/{search}', function ($search) use ($app) {
 
 $app->get('/rx.json', function () use ($app) {
   $rx_names = array();
-  if($app['request']->get('search')){
-    $term = $app['request']->get('search');
-    return queryFDA($med);
-  }
-  elseif($app['request']->get('ahrq')){
-    $result = new ahrq();
+  if ($app['request']->get('ahrq')) {
+    $result = new stdClass();
     $term = $app['request']->get('ahrq');
-     $result->ahrq_sample = ahrq($term);
-     return json_encode($result);
+      $result->ahrq_sample = ahrqCount($app, $term);
+      return json_encode($result);
   }
-  else{
+  else {
     $rx_name_sql = "SELECT rx_name FROM rx_names";
     $rx_names_results = $app['db']->fetchAll($rx_name_sql);
-    foreach($rx_names_results as $rx_name){
-      $rx_names[] = $rx_name['rx_name']; 
+    foreach ($rx_names_results as $rx_name) {
+      $rx_names[] = $rx_name['rx_name'];
     }
     return json_encode($rx_names);
   }
 });
-
-/*
- * TO DO.
- * 
-  function combinedData($med){
-    $client = new Client([
-        // Base URI is used with relative requests
-        'base_uri' => 'https://api.fda.gov/',
-        // You can set any number of default request options.
-        'timeout'  => 2.0,
-    ]);
-
-    $response = $client->get('https://api.fda.gov/drug/event.json', [
-      'query' => [
-        'search' => $med,
-        'count' => 'patient.reaction.reactionmeddrapt.exact']
-    ]);
-    $data = json_decode((string) $response->getBody(), TRUE);
-
-    foreach($data['results'] as $result){
-        $html .= '<div><b>Term: </b> ' . $result['term'] . '</div>';
-        $html .= '<div><b>Reports: </b> ' . $result['count'] . '</div>';
-    }
-
-    return $html;
-  }
-
-  function normalize($fda_count, $ahrq_count){
-    //$normalized = ($ahrq_count / $fda_count);
-    return $normalized;
-  }
- *
- */
 
 $app->run();
